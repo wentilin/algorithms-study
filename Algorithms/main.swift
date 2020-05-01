7833151105ae2b7f66f2b429a2f11a26f1428984
linwh <linwh@adnonstop.com>
2020-05-01 11:47:18 +0800

    add GreedyMoney Algorithm

--- Algorithms/main.swift
+++ Algorithms/main.swift
@@ -38,3 +38,10 @@ print(arr)
 print(search_nxm(arr, number: 4))
 print(indexOfArryNxM(arr, target: 4))
 print(minNumberInRotateArray([3, 4, 2, 3]))
+
+let moneyMaps: [MoneyMap] = [.init(kind: 1, count: 5),
+                             .init(kind: 5, count: 2),
+                             .init(kind: 10, count: 2),
+                             .init(kind: 50, count: 3),
+                             .init(kind: 100, count: 5)]
+print(GreedyMoney.solve(money: 456, moneyMaps: moneyMaps))
