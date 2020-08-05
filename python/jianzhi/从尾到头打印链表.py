a6cb7091129aeccf14aacb55b0ba7a53bab93f64
fionattu <includetujy@gmail.com>
2020-08-05 18:39:15 +0800

    reverse linkedlist; print reversed linkedlist and etc.

---
+++ python/jianzhi/从尾到头打印链表.py
@@ -0,0 +1,32 @@
+"""
+题目描述
+输入一个链表，按链表从尾到头的顺序返回一个ArrayList。
+"""
+
+
+# -*- coding:utf-8 -*-
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    # 方法: 递归
+    # time: O(n)
+    # space: O(n)
+    def printListFromTailToHead(self, listNode):
+        store = []
+        if not listNode:
+            return store
+        store += self.printListFromTailToHead(listNode.next)
+        store.append(listNode.val)
+        return store
+
+
+if __name__ == '__main__':
+    a1 = ListNode(1)
+    a1.next = ListNode(3)
+    a1.next.next = ListNode(5)
+
+    print(Solution().printListFromTailToHead(a1))
---
+++ python/jianzhi/反转链表.py
@@ -0,0 +1,40 @@
+"""
+题目描述
+输入一个链表，反转链表后，输出新链表的表头。
+"""
+# -*- coding:utf-8 -*-
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    # 方法: 遍历
+    # time: O(n)
+    # space: O(1)
+    def ReverseList(self, pHead):
+        if not pHead or not pHead.next:
+            return pHead
+
+        cur = pHead
+        prev = None
+
+        while cur:
+            tmp = cur.next  # 要先把next存起来
+            cur.next = prev  # 否则这一行会把原序列打乱
+            prev = cur
+            cur = tmp
+        return prev
+
+
+if __name__ == '__main__':
+    a1 = ListNode(1)
+    a1.next = ListNode(3)
+    a1.next.next = ListNode(5)
+
+    test = Solution().ReverseList(a1)
+
+    while test:
+        print(test.val)
+        test = test.next
