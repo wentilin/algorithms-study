485e206af7c79b1bf15f05f99f3e1d096748a6b7
wentilin <837956177@qq.com>
2020-09-30 19:10:36 +0800

    merge two ordered listNode recursive

--- swift/Algorithms/ListNode/ListNodeAlgorithm.swift
+++ swift/Algorithms/ListNode/ListNodeAlgorithm.swift
@@ -86,6 +86,23 @@ class ListNodeAlgorithm {
         return head.next
     }
     
+    /// 递归合并两个有序链表
+    static func mergeRecursive(list1: ListNode?, list2: ListNode?) -> ListNode? {
+        if list1 == nil {
+            return list2
+        }
+        if list2 == nil {
+            return list1
+        }
+        
+        if list1!.val <= list2!.val {
+            list1?.next = mergeRecursive(list1: list1?.next, list2: list2)
+            return list1
+        } else {
+            list2?.next = mergeRecursive(list1: list1, list2: list2?.next)
+            return list2
+        }
+    }
     /// 输入一个复杂链表（每个节点中有节点值，以及两个指针，一个指向下一个节点，
     /// 另一个特殊指针random指向一个随机节点），请对此链表进行深拷贝，
     /// 并返回拷贝后的头结点。（注意，输出结果中请不要返回参数中的节点引用，否则判题程序会直接返回空）
