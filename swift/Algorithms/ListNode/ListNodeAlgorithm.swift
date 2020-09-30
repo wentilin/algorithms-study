4f7656b9ac0a9bdeb15c6710f27a1227580937f2
wentilin <837956177@qq.com>
2020-09-30 20:47:28 +0800

    旋转链表k次

--- swift/Algorithms/ListNode/ListNodeAlgorithm.swift
+++ swift/Algorithms/ListNode/ListNodeAlgorithm.swift
@@ -326,3 +326,42 @@ extension ListNodeAlgorithm {
         return mergeTwoLists(list1: mergeList(lists, lo: lo, hi: mid), list2: mergeList(lists, lo: mid+1, hi: hi))
     }
 }
+
+extension ListNodeAlgorithm {
+    /**
+     题目：旋转链表k次
+     描述：给定一个链表，旋转链表，将链表每个节点向右移动 k 个位置，其中 k 是非负数。
+     示例：输入: 0->1->2->NULL, k = 4
+          输出: 2->0->1->NULL
+          解释:
+          向右旋转 1 步: 2->0->1->NULL
+          向右旋转 2 步: 1->2->0->NULL
+          向右旋转 3 步: 0->1->2->NULL
+          向右旋转 4 步: 2->0->1->NULL
+     解法：将尾部next指向头部形成环，在从头部遍历，len - k % len - 1处是新的头部，前一个是新的尾部
+     */
+    static func rotateRight(_ head: ListNode?, _ k: Int) -> ListNode? {
+        if head?.next == nil {
+            return head
+        }
+
+        var tail: ListNode? = head
+        var len = 1
+        while tail?.next != nil {
+            tail = tail?.next
+            len += 1
+        }
+
+        tail?.next = head
+
+        var newHead: ListNode? = head?.next
+        tail = head
+        for _ in 0..<(len - k % len - 1) {
+            newHead = newHead?.next
+            tail = tail?.next
+        }
+
+        tail?.next = nil
+        return newHead
+    }
+}
