8b684ac8bc2b650b1fd9792eeff1e7cf964dc55d
wentilin <837956177@qq.com>
2020-10-18 11:32:13 +0800

    删除链表的倒数第n个节点

--- swift/Algorithms/ListNode/ListNodeAlgorithm.swift
+++ swift/Algorithms/ListNode/ListNodeAlgorithm.swift
@@ -365,3 +365,43 @@ extension ListNodeAlgorithm {
         return newHead
     }
 }
+
+extension ListNodeAlgorithm {
+    /**
+     题目：删除链表的倒数第n个节点
+     描述：给定一个链表，删除链表的倒数第n个节点并返回链表的头指针
+          例如，
+            给出的链表为:1->2->3->4->5, n= 2.
+            删除了链表的倒数第n个节点之后,链表变为1->2->3->5.
+          备注：
+          题目保证n一定是有效的
+          请给出请给出时间复杂度为\ O(n) O(n)的算法
+     */
+    static func removeNthFromEnd(_ head: ListNode?, n: Int) -> ListNode? {
+        if head == nil {
+            return nil
+        }
+        
+        let dummyHead = ListNode(val: 0)
+        
+        var p: ListNode? = dummyHead
+        var q: ListNode? = dummyHead
+        for _ in 0...n {
+            q = q?.next
+        }
+        
+        while q != nil {
+            q = q?.next
+            p = p?.next
+        }
+        
+        let delNode = p?.next
+        p?.next = delNode?.next
+        delNode?.next = nil
+        
+        let h = dummyHead.next
+        dummyHead.next = nil
+        
+        return h
+    }
+}
